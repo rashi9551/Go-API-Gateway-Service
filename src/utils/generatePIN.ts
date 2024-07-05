4765b62553af74d8857199d9536e6684ad8d1b17
rashi9551 <rashidroz1361@gmail.com>
2024-07-05 14:28:51 +0530

    start rating imlplemented and feedback

--- src/utils/generatePIN.ts
+++ src/utils/generatePIN.ts
@@ -3,6 +3,8 @@ export const generatePIN = (): number => {
     for (let i = 0; i < 6; i++) {
       pin += Math.floor(Math.random() * 10);
     }
+    console.log(pin,"ithu ride pin");
+    
     return parseInt(pin);
   };
   
